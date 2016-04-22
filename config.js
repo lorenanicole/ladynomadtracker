23d76d39daf7f446520b5bc689ae71a3f5b13989
Lorena Mesa <me@lorenamesa.com>
2016-04-22 14:25:53 -0500

    Add config file in again

---
+++ config.js
@@ -0,0 +1,4 @@
+var config = {};
+config.googleMapsApiKey = 'AIzaSyA4AQA_ZJpqGeHF5S2d4T1wii7mgeJD2QI';
+config.firebaseUrl = 'https://resplendent-torch-9494.firebaseio.com';
+module.exports = config;
